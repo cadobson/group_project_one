19484a2b228f6f82ee0ca80fe8f750194f2e3584
christopher <christopher.anthony.dobson@gmail.com>
2023-02-01 19:26:06 -0500

    implement basic questions store

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import questionsReducer from './questions';
 import session from './session'
 
 const rootReducer = combineReducers({
   session,
+  questions: questionsReducer
 });
 
 
