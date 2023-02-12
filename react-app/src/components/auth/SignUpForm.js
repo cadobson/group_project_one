ea11b17cac889ba5831eb4a655858e48660ebca5
Ray Yu <RayYu@Lucky.com>
2023-02-12 15:01:52 -0500

    sign-up validation

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -72,7 +72,7 @@ const SignUpForm = () => {
       <div>
         <label>Email</label>
         <input
-          type='text'
+          type='email'
           name='email'
           onChange={updateEmail}
           value={email}
