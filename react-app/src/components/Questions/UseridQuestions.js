5e1bc49f1af358f51f53193e13321a2bb1049549
christopher <christopher.anthony.dobson@gmail.com>
2023-02-12 17:59:42 -0500

    clean up questions belonging to users

--- react-app/src/components/Questions/UseridQuestions.js
+++ react-app/src/components/Questions/UseridQuestions.js
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams, useHistory } from "react-router-dom"
 import { loadQuestionsByUserId } from "../../store/questions"
+import OneQuestion from "./OneQuestion"
 
 export const UseridQuestions = () => {
     const dispatch = useDispatch();
     const { userId } = useParams()
 
     const allQuestions = useSelector(state => state?.questions)
-
-    console.log(allQuestions, ' this is all questions')
 
     useEffect(() => {
         dispatch(loadQuestionsByUserId(userId))
@@ -18,9 +17,10 @@ export const UseridQuestions = () => {
     return (
         <div>
             {allQuestions && allQuestions?.Questions?.map(ele => {
-                return <a href={`/questions/${ele.id}`}>
-                    <li key={ele.id}>{ele.title}</li>
-                </a>
+                // return <a href={`/questions/${ele.id}`}>
+                //     <li key={ele.id}>{ele.title}</li>
+                // </a>
+                return <OneQuestion key={ele.id} question={ele} />
             })}
         </div>
     )
